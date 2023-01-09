d83d7874b342dcaadd8d7b487b1f6038813c5481
mikhail veselov <mishaveselov@yandex.ru>
2023-01-09 11:10:49 +0300

    Первая задача с двумя числами

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 2;
+int b = 10;
+int max = a;
+int min = b;
+
+if (a > b) max = a;
+if (b > a) max = b;
+
+if (a < b) min = a;
+if (b < a) min = b;
+
+
+Console.Write("max = ");
+Console.WriteLine(max); 
+
+Console.Write("min = ");
+Console.WriteLine(min);
