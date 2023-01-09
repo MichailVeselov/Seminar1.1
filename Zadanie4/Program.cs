2f0845e478031a11b6d08feba143ff29b9ec1eac
mikhail veselov <mishaveselov@yandex.ru>
2023-01-09 15:05:12 +0300

    Четвертая задача с числами

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array =  {1,2,3,4,5};
+int n = 5;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] % 2 == 0)
+    {
+        Console.WriteLine (array[index]);
+    }
+    
+    index ++;
+}
